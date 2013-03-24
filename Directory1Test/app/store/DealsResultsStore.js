2c39020868992c6dd62f23ff59e39fd084bf2f7b
directory1@business2mobile.com <directory1@business2mobile.com>
2013-03-24 02:57:01 -0500

    a

---
+++ Directory1Test/app/store/DealsResultsStore.js
@@ -0,0 +1,29 @@
+﻿Ext.define('Mobile.store.DealsResultsStore', {
+    extend: 'Ext.data.Store',
+
+    config: {
+        model: 'Mobile.model.DealsResultsModel',
+        //sorters: 'Name',
+
+        pageSize: 10,
+        autoLoad: false,
+
+        proxy: {
+            type: 'ajax',
+            url: 'http://directory1.business2mobile.com/DesktopModules/WebApiB2M/API/Directory1/DealsDataFeedGet',
+
+            pageParam: 'page',
+            limitParam: 'rpp',
+            
+            extraParams: {
+                q: 'featured'
+            },
+
+            method: 'get',
+
+            reader: {
+                type: 'json'
+            }
+        }
+    }
+});
---
+++ Directory1Test/app/store/ListingsResultsStore.js
@@ -0,0 +1,29 @@
+﻿Ext.define('Mobile.store.ListingsResultsStore', {
+    extend: 'Ext.data.Store',
+
+    config: {
+        model: 'Mobile.model.ListingsResultsModel',
+        //sorters: 'Name',
+
+        pageSize: 10,
+        autoLoad: false,
+
+        proxy: {
+            type: 'ajax',
+            url: 'http://directory1.business2mobile.com/DesktopModules/WebApiB2M/API/Directory1/ListingsDataFeedGet',
+
+            pageParam: 'page',
+            limitParam: 'rpp',
+            
+            extraParams: {
+                q: 'featured'
+            },
+
+            method: 'get',
+
+            reader: {
+                type: 'json'
+            }
+        }
+    }
+});
